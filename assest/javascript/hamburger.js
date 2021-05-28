a790e9c70eda1c493fda707dd21220ca67dc8ac4
Alabi Abayomi <abayomi15@gmail.com>
2021-05-28 02:09:38 +0100

    fixed hambugger errors

--- assest/javascript/hamburger.js
+++ assest/javascript/hamburger.js
@@ -1,19 +1,20 @@
-const btnHam = document.querySelector(".ham-btn");
-const btnTimes = document.querySelector(".times-btn");
-const navBar = document.getElementById("nav-bar");
 
-btnHam.addEventListener("click", function () {
+const btnHam = document.querySelector('.ham-btn');
+const btnTimes = document.querySelector('.times-btn');
+const navBar = document.getElementById('nav-bar');
+
+btnHam.addEventListener('click', function(){
     if(btnHam.className !== ""){
         btnHam.style.display = "none";
         btnTimes.style.display = "block";
-        navBar.classList.add(".show-btn");
+        navBar.classList.add("show-nav");
     }
 })
 
-btnTimes.addEventListener("click", function name() {
-    if (btnHam.className !== "") {
+btnTimes.addEventListener('click', function(){
+    if(btnHam.className !== ""){
         this.style.display = "none";
         btnHam.style.display = "block";
-        navBar.classList.remove("show-btn");
+        navBar.classList.remove("show-nav");
     }
 })
